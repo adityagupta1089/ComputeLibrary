c045f85f6c1708ee39de9f44b5efdf5127deec55
Aditya Gupta <adityagupta1089@users.noreply.github.com>
2019-04-17 06:41:21 +0530

    Update graph_resnet50_2.cpp

--- examples/graph_resnet50_2.cpp
+++ examples/graph_resnet50_2.cpp
@@ -43,17 +43,15 @@ static unsigned int inferences = 0;
  * @param[in] argc Number of arguments
  * @param[in] argv Arguments
  */
-class GraphAlexnetExample : public Example
+class GraphResNeXt50Example : public Example
 {
 public:
     
 
-    GraphAlexnetExample()
-        : cmd_parser(), common_opts(cmd_parser), common_params(), graph(0, "AlexNet")
+    GraphResNeXt50Example()
+        : cmd_parser(), common_opts(cmd_parser), common_params(), graph(0, "ResNeXt50")
     {
     }
-    
-    static std::mutex finalize_mutex;
     
     bool do_setup(int argc, char **argv) override
     {
@@ -70,12 +68,6 @@ public:
             return false;
         }
 
-        // Set default layout if needed
-        if(!common_opts.data_layout->is_set() && common_params.target == Target::NEON)
-        {
-            common_params.data_layout = DataLayout::NCHW;
-        }
-
         // Checks
         ARM_COMPUTE_EXIT_ON_MSG(arm_compute::is_data_type_quantized_asymmetric(common_params.data_type), "QASYMM8 not supported for this graph");
 
@@ -85,12 +77,8 @@ public:
         // Get trainable parameters data path
         std::string data_path = common_params.data_path;
 
-        // Create a preprocessor object
-        const std::array<float, 3> mean_rgb{ { 122.68f, 116.67f, 104.01f } };
-        std::unique_ptr<IPreprocessor> preprocessor = arm_compute::support::cpp14::make_unique<CaffePreproccessor>(mean_rgb);
-
         // Create input descriptor
-        const TensorShape tensor_shape     = permute_shape(TensorShape(227U, 227U, 3U, 1U), DataLayout::NCHW, common_params.data_layout);
+        const TensorShape tensor_shape     = permute_shape(TensorShape(224U, 224U, 3U, 1U), DataLayout::NCHW, common_params.data_layout);
         TensorDescriptor  input_descriptor = TensorDescriptor(tensor_shape, common_params.data_type).set_layout(common_params.data_layout);
 
         // Set weights trained layout
@@ -98,82 +86,36 @@ public:
 
         graph << common_params.target
               << common_params.fast_math_hint
-              << InputLayer(input_descriptor, get_input_accessor(common_params, std::move(preprocessor)))
-              // Layer 1
+              << InputLayer(input_descriptor, get_input_accessor(common_params))
+              << ScaleLayer(get_weights_accessor(data_path, "/cnn_data/resnext50_model/bn_data_mul.npy"),
+                            get_weights_accessor(data_path, "/cnn_data/resnext50_model/bn_data_add.npy"))
+              .set_name("bn_data/Scale")
               << ConvolutionLayer(
-                  11U, 11U, 96U,
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/conv1_w.npy", weights_layout),
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/conv1_b.npy"),
-                  PadStrideInfo(4, 4, 0, 0))
-              .set_name("conv1")
-              << ActivationLayer(ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::RELU)).set_name("relu1")
-              << NormalizationLayer(NormalizationLayerInfo(NormType::CROSS_MAP, 5, 0.0001f, 0.75f)).set_name("norm1")
-              << PoolingLayer(PoolingLayerInfo(PoolingType::MAX, 3, PadStrideInfo(2, 2, 0, 0))).set_name("pool1")
-              // Layer 2
-              << ConvolutionLayer(
-                  5U, 5U, 256U,
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/conv2_w.npy", weights_layout),
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/conv2_b.npy"),
-                  PadStrideInfo(1, 1, 2, 2), 2)
-              .set_name("conv2")
-              << ActivationLayer(ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::RELU)).set_name("relu2")
-              << NormalizationLayer(NormalizationLayerInfo(NormType::CROSS_MAP, 5, 0.0001f, 0.75f)).set_name("norm2")
-              << PoolingLayer(PoolingLayerInfo(PoolingType::MAX, 3, PadStrideInfo(2, 2, 0, 0))).set_name("pool2")
-              // Layer 3
-              << ConvolutionLayer(
-                  3U, 3U, 384U,
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/conv3_w.npy", weights_layout),
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/conv3_b.npy"),
-                  PadStrideInfo(1, 1, 1, 1))
-              .set_name("conv3")
-              << ActivationLayer(ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::RELU)).set_name("relu3")
-              // Layer 4
-              << ConvolutionLayer(
-                  3U, 3U, 384U,
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/conv4_w.npy", weights_layout),
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/conv4_b.npy"),
-                  PadStrideInfo(1, 1, 1, 1), 2)
-              .set_name("conv4")
-              << ActivationLayer(ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::RELU)).set_name("relu4")
-              // Layer 5
-              << ConvolutionLayer(
-                  3U, 3U, 256U,
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/conv5_w.npy", weights_layout),
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/conv5_b.npy"),
-                  PadStrideInfo(1, 1, 1, 1), 2)
-              .set_name("conv5")
-              << ActivationLayer(ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::RELU)).set_name("relu5")
-              << PoolingLayer(PoolingLayerInfo(PoolingType::MAX, 3, PadStrideInfo(2, 2, 0, 0))).set_name("pool5")
-              // Layer 6
-              << FullyConnectedLayer(
-                  4096U,
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/fc6_w.npy", weights_layout),
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/fc6_b.npy"))
-              .set_name("fc6")
-              << ActivationLayer(ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::RELU)).set_name("relu6")
-              // Layer 7
-              << FullyConnectedLayer(
-                  4096U,
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/fc7_w.npy", weights_layout),
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/fc7_b.npy"))
-              .set_name("fc7")
-              << ActivationLayer(ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::RELU)).set_name("relu7")
-              // Layer 8
-              << FullyConnectedLayer(
-                  1000U,
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/fc8_w.npy", weights_layout),
-                  get_weights_accessor(data_path, "/cnn_data/alexnet_model/fc8_b.npy"))
-              .set_name("fc8")
-              // Softmax
-              << SoftmaxLayer().set_name("prob")
-              << OutputLayer(get_output_accessor(common_params, 5));
+                  7U, 7U, 64U,
+                  get_weights_accessor(data_path, "/cnn_data/resnext50_model/conv0_weights.npy", weights_layout),
+                  get_weights_accessor(data_path, "/cnn_data/resnext50_model/conv0_biases.npy"),
+                  PadStrideInfo(2, 2, 2, 3, 2, 3, DimensionRoundingType::FLOOR))
+              .set_name("conv0/Convolution")
+              << ActivationLayer(ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::RELU)).set_name("conv0/Relu")
+              << PoolingLayer(PoolingLayerInfo(PoolingType::MAX, 3, PadStrideInfo(2, 2, 0, 1, 0, 1, DimensionRoundingType::FLOOR))).set_name("pool0");
+
+        add_residual_block(data_path, weights_layout, /*ofm*/ 256, /*stage*/ 1, /*num_unit*/ 3, /*stride_conv_unit1*/ 1);
+        add_residual_block(data_path, weights_layout, 512, 2, 4, 2);
+        add_residual_block(data_path, weights_layout, 1024, 3, 6, 2);
+        add_residual_block(data_path, weights_layout, 2048, 4, 3, 2);
+
+        graph << PoolingLayer(PoolingLayerInfo(PoolingType::AVG)).set_name("pool1")
+              << FlattenLayer().set_name("predictions/Reshape")
+              << OutputLayer(get_npy_output_accessor(common_params.labels, TensorShape(2048U), DataType::F32));
 
         // Finalize graph
         GraphConfig config;
         config.num_threads = common_params.threads;
         config.use_tuner   = common_params.enable_tuner;
         config.tuner_file  = common_params.tuner_file;
+
         graph.finalize(common_params.target, config);
+
         return true;
     }
     void do_run() override
@@ -195,11 +137,77 @@ struct _config {
     std::string                 name;
     int                         argc;
     std::vector<std::string>    argv;
+	
+
+    void add_residual_block(const std::string &data_path, DataLayout weights_layout,
+                            unsigned int base_depth, unsigned int stage, unsigned int num_units, unsigned int stride_conv_unit1)
+    {
+        for(unsigned int i = 0; i < num_units; ++i)
+        {
+            std::stringstream unit_path_ss;
+            unit_path_ss << "/cnn_data/resnext50_model/stage" << stage << "_unit" << (i + 1) << "_";
+            std::string unit_path = unit_path_ss.str();
+
+            std::stringstream unit_name_ss;
+            unit_name_ss << "stage" << stage << "/unit" << (i + 1) << "/";
+            std::string unit_name = unit_name_ss.str();
+
+            PadStrideInfo pad_grouped_conv(1, 1, 1, 1);
+            if(i == 0)
+            {
+                pad_grouped_conv = (stage == 1) ? PadStrideInfo(stride_conv_unit1, stride_conv_unit1, 1, 1) : PadStrideInfo(stride_conv_unit1, stride_conv_unit1, 0, 1, 0, 1, DimensionRoundingType::FLOOR);
+            }
+
+            SubStream right(graph);
+            right << ConvolutionLayer(
+                      1U, 1U, base_depth / 2,
+                      get_weights_accessor(data_path, unit_path + "conv1_weights.npy", weights_layout),
+                      get_weights_accessor(data_path, unit_path + "conv1_biases.npy"),
+                      PadStrideInfo(1, 1, 0, 0))
+                  .set_name(unit_name + "conv1/convolution")
+                  << ActivationLayer(ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::RELU)).set_name(unit_name + "conv1/Relu")
+
+                  << ConvolutionLayer(
+                      3U, 3U, base_depth / 2,
+                      get_weights_accessor(data_path, unit_path + "conv2_weights.npy", weights_layout),
+                      std::unique_ptr<arm_compute::graph::ITensorAccessor>(nullptr),
+                      pad_grouped_conv, 32)
+                  .set_name(unit_name + "conv2/convolution")
+                  << ScaleLayer(get_weights_accessor(data_path, unit_path + "bn2_mul.npy"),
+                                get_weights_accessor(data_path, unit_path + "bn2_add.npy"))
+                  .set_name(unit_name + "conv1/Scale")
+                  << ActivationLayer(ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::RELU)).set_name(unit_name + "conv2/Relu")
+
+                  << ConvolutionLayer(
+                      1U, 1U, base_depth,
+                      get_weights_accessor(data_path, unit_path + "conv3_weights.npy", weights_layout),
+                      get_weights_accessor(data_path, unit_path + "conv3_biases.npy"),
+                      PadStrideInfo(1, 1, 0, 0))
+                  .set_name(unit_name + "conv3/convolution");
+
+            SubStream left(graph);
+            if(i == 0)
+            {
+                left << ConvolutionLayer(
+                         1U, 1U, base_depth,
+                         get_weights_accessor(data_path, unit_path + "sc_weights.npy", weights_layout),
+                         std::unique_ptr<arm_compute::graph::ITensorAccessor>(nullptr),
+                         PadStrideInfo(stride_conv_unit1, stride_conv_unit1, 0, 0))
+                     .set_name(unit_name + "sc/convolution")
+                     << ScaleLayer(get_weights_accessor(data_path, unit_path + "sc_bn_mul.npy"),
+                                   get_weights_accessor(data_path, unit_path + "sc_bn_add.npy"))
+                     .set_name(unit_name + "sc/scale");
+            }
+
+            graph << BranchLayer(BranchMergeMethod::ADD, std::move(left), std::move(right)).set_name(unit_name + "add");
+            graph << ActivationLayer(ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::RELU)).set_name(unit_name + "Relu");
+        }
+}
 };
 
 static std::atomic_uint* val;
 
-/** Main program for AlexNet
+/** Main program for ResNeXt50
  *
  * @note To list all the possible arguments execute the binary appended with the --help option
  *
@@ -272,7 +280,7 @@ int main(int argc, char **argv)
                     //std::cout << config.name << " : " << (*val)++ << std::endl;
                     processed++;
                     (*val)++;
-                    arm_compute::utils::run_example<GraphAlexnetExample>(config.argc, argv); 
+                    arm_compute::utils::run_example<GraphResNeXt50Example>(config.argc, argv); 
                 }
                 std::cout << "Completed " << config.name << ": " << processed << " inferences" << std::endl;
                 exit(1);
