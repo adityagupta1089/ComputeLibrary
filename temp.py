7a275cfbcbea6593a46cd9fd15191b6d2648da2a
Aditya Gupta <adityagupta1089@gmail.com>
2019-05-01 16:04:33 +0000

    Add few temp plots

--- temp.py
+++ temp.py
@@ -23,15 +23,15 @@ sm = ["taskset", "-c", "0-3"]
 bg = ["taskset", "-c", "4-7"]
 
 targets = {
-    "cpu big": lambda g: bg + [pp + g] + cp,
-    "cpu small": lambda g: sm + [pp + g] + cp,
+    "cpu_big": lambda g: bg + [pp + g] + cp,
+    "cpu_small": lambda g: sm + [pp + g] + cp,
     "gpu": lambda g: [pp + g] + gp
 }
 
 T = 5
 dt = 0.1
 
-temp_func = lambda t, q0, c, v, r: q0 + c * v * (1 - np.exp(- t / (r * c)))
+temp_func = lambda t, q0, qi, tau: q0 + qi * (1 - np.exp(- t / tau))
 
 
 def get_temp():
@@ -40,24 +40,30 @@ def get_temp():
         return temp
 
 def plot_temp(graph, target, x, y, popt, i):
-    plt.figure()
+    fig = plt.figure()
     plt.plot(x, y)
     plt.plot(x, temp_func(x, *popt), 
         'r-', 
-        label='fit: Q0=%5.3f, C=%5.3f, V=%5.3f, R=%5.3f' % tuple(popt))
+        label='fit: Q0=%5.3f, Qinf=%5.3f, Tau=%5.3f' % tuple(popt))
     plt.axhline(65000, linestyle='--', color='r')
     plt.xlabel('Time')
     plt.ylabel('Temperature')
     plt.title('Execution ' + graph + " " + target)
     plt.legend(loc='best')
     plt.savefig('temp_plots/' + graph + "_" + target + "_" + str(i) + ".png")  
+    plt.close(fig)
 
-def getRC(graph, target, x, y, i):
-    popt, pcov = curve_fit(temp_func, x, y, bounds=(0, np.inf))
-    print('> Q0=%5.3f, C=%5.3f, V=%5.3f, R=%5.3f' % tuple(popt))
-    plot_temp(graph, target, x, y, popt, i)
+def get_params(graph, target, x, y, i):
+    try:
+        popt, _ = curve_fit(temp_func, x, y, bounds=(0, np.inf))
+        print('> Q0=%5.3f, Qinf=%5.3f, Tau=%5.3f' % tuple(popt))
+        plot_temp(graph, target, x, y, popt, i)
+        return popt
+    except:
+        print("> Bad Graph")
+        return []
        
-def findRC(graph, target, cmd, i):
+def find_params(graph, target, cmd, i):
     x = []
     y = []
     t = 0
@@ -70,17 +76,15 @@ def findRC(graph, target, cmd, i):
         t += dt
     x = np.array(x)
     y = np.array(y)
-    return getRC(graph, target, x, y, i)
+    return get_params(graph, target, x, y, i)
             
 if __name__ == "__main__":
     env = dict(os.environ)
     env['LD_LIBRARY_PATH'] = './build/release'
     RCs = {}
-    for graph in graphs[:1]:
+    for graph in graphs:
         print(graph)
         for target in targets:
             print(target)
-            for i in range(1):
-                print("Iteration", i)
-                RCs[(graph, target)] = findRC(graph, target, targets[target](graph), i)
-            break
+            for i in range(5):
+                RCs[(graph, target)] = find_params(graph, target, targets[target](graph), i)
