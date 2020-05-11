8fca11943c9c09b64eaad919aa8ffd2a0d0b3ae8
Aditya Gupta <adityagupta1089@gmail.com>
2020-05-11 06:08:02 +0000

    Add a plot for v4

--- temp_schedulerv3/plot.py
+++ temp_schedulerv3/plot.py
@@ -3,7 +3,7 @@ import os
 import re
 
 
-def plot_temp(TL):
+def plot_temp(TL, dt):
     plt.figure()
     plt.plot(x, y)
     plt.axhline(80000, linestyle='--', color='r')
@@ -11,16 +11,18 @@ def plot_temp(TL):
         plt.axhline(TL, linestyle='--', color='g')
     plt.xlabel('Time')
     plt.ylabel('Temperature')
-    plt.title(f'Execution scheduler v3 (TL={TL})')
-    plt.savefig(f'resnet50_TL{TL}.png')
+    plt.title('Execution scheduler v4 (TL='+str(TL)+',dt='+str(dt)+'')
+    plt.savefig('resnet50_TL'+str(TL)+'_dt'+str(dt)+'.png')
 
 
 for file in os.listdir():
-    if re.match(r"resnet50_TL\d+.csv", file):
+    if re.match(r"resnet50_TL\d+_dt\d+.csv", file):
         with open(file, 'r') as f:
             content = f.readlines()
-        tl = int(re.findall(r"resnet50_TL(\d+).csv", file)[0])
-        print(tl)
+        tl, dt = re.findall(r"resnet50_TL(\d+)_dt(\d+).csv", file)[0]
+        tl = int(tl)
+        dt = int(dt)
+        print(tl, dt)
 
         x = []
         y = []
@@ -29,4 +31,4 @@ for file in os.listdir():
             x.append(float(xx))
             y.append(float(yy))
 
-        plot_temp(tl)
+        plot_temp(tl, dt)
