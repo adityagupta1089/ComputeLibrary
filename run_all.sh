585b1a9401882182d475c42498b6ad1129e9fd4d
Aditya Gupta <adityagupta1089@gmail.com>
2020-07-22 11:47:41 +0000

    Perform experiments
    - Temp profiling for (100, 10)
    - Scheduler for the same

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,17 @@
+# Time profiling
+
 # for graph in alexnet googlenet mobilenet resnet50 squeezenet; do
 #     ./build/release/examples/graph_temp_scheduler --graph=$graph --profile-time
 # done
+
+# Temp profiling
+
+# for graph in alexnet googlenet mobilenet resnet50 squeezenet; do
+#     ./build/release/examples/graph_temp_scheduler --graph=$graph --profile-temp
+# done
+
+# Scheduling
+
 for graph in alexnet googlenet mobilenet resnet50 squeezenet; do
     for version in 3 3.1 4; do
         for tl in 80000 999999; do
