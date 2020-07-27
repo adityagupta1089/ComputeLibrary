b247d2e584f64db0549c00fd655ac034e3ea71b0
Aditya Gupta <adityagupta1089@gmail.com>
2020-07-27 14:43:45 +0000

    Add missing logs

--- run_all.sh
+++ run_all.sh
@@ -12,8 +12,8 @@
 
 # Scheduling
 
-for graph in alexnet googlenet mobilenet resnet50 squeezenet; do
-    for version in 3 3.1 4; do
+for graph in squeezenet; do
+    for version in 3.1; do
         for tl in 80000 999999; do
             ./build/release/examples/graph_temp_scheduler --graph=$graph --run-sched --version=$version --tl=$tl
         done
