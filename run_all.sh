25dc317ccc99ee9871179653d892cb73ff4e185d
Aditya Gupta <adityagupta1089@gmail.com>
2020-09-18 05:57:40 +0000

    Update TL

--- run_all.sh
+++ run_all.sh
@@ -12,9 +12,9 @@
 
 # Scheduling
 
-for graph in squeezenet; do
-    for version in 3.1; do
-        for tl in 80000 999999; do
+for graph in alexnet googlenet mobilenet resnet50 squeezenet; do
+    for version in 3 3.1 4; do
+        for tl in 85000 999999; do
             ./build/release/examples/graph_temp_scheduler --graph=$graph --run-sched --version=$version --tl=$tl
         done
     done
